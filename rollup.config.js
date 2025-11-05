f69c97cc870ad6bc6bd5ec183412f1c672b51d0d
938134 <github-actions[bot]@users.noreply.github.com>
2025-11-05 20:47:50 +0800

    update 20251105

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'src/main.js',
+  input: 'src/main.js',  
   output: {
     file: 'ha-cardforge-card.js',
-    format: 'es'
+    format: 'es',
   },
   external: [
     /^lit/,
     /^@lit/,
     'lit-element',
-    'lit-html',
+    'lit-html', 
     'lit-html/is-server',
   ],
   plugins: [
