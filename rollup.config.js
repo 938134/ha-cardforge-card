d3b5e0b8e383e4ed087d61466dbf07bc2e8928de
938134 <github-actions[bot]@users.noreply.github.com>
2025-11-09 12:30:49 +0800

    update 20251109

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/main.js',  
   output: {
-    file: 'ha-cardforge-card.js',
+    file: 'dist/ha-cardforge-card.js',
     format: 'es',
   },
   external: [
@@ -15,5 +16,10 @@ export default {
   ],
   plugins: [
     nodeResolve({ preferBuiltins: false }),
+    copy({
+      targets: [
+        { src: 'src/plugins/*', dest: 'dist/plugins/' }
+      ]
+    })
   ],
 };
