a5b8533a81cf08f22e0339ead0abcfb917aaf5ae
蜘蛛饭 <39700758+938134@users.noreply.github.com>
2025-11-14 12:33:47 +0800

    Delete src/core/index.js

--- src/core/index.js
+++
@@ -1,5 +0,0 @@
-// src/core/index.js
-export { HaCardForgeCard } from './ha-cardforge-card.js';
-export { BasePlugin } from './base-plugin.js';
-export { PluginRegistry } from './plugin-registry.js';
-export * from './utils.js';
