9c181a75ca44ec8b564d8210371fa58bc847cf20
938134 <github-actions[bot]@users.noreply.github.com>
2025-11-16 08:43:10 +0800

    update 20251116

--- src/ha-cardforge-card.js
+++ src/ha-cardforge-card.js
@@ -174,7 +174,7 @@ class HaCardForgeCard extends LitElement {
 
   static getStubConfig() {
     return {
-      plugin: 'simple-clock',
+      plugin: 'clock-card',
       entities: {},
       theme: 'auto'
     };
