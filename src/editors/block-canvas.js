4809167827729a428f3606cea8dc42d47c85e2a0
蜘蛛饭 <39700758+938134@users.noreply.github.com>
2025-11-25 13:18:29 +0800

    Update block-canvas.js

--- src/editors/block-canvas.js
+++ src/editors/block-canvas.js
@@ -1,7 +1,7 @@
 // src/editors/layout-canvas.js
 import { LitElement, html, css } from 'https://unpkg.com/lit@2.8.0/index.js?module';
 import { designSystem } from '../core/design-system.js';
-import { blockRegistry } from '../core/block-registry.js';
+import { blockManager } from '../core/block-manager.js';
 
 class LayoutCanvas extends LitElement {
   static properties = {
