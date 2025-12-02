b51207a4d0b6a380136ba7b4a3b0c352a8e381b5
蜘蛛饭 <39700758+938134@users.noreply.github.com>
2025-12-02 16:04:12 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,15 @@
 // rollup.config.js
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-// rollup.config.js
 export default {
   input: 'src/main.js',
   output: {
     file: 'dist/ha-cardforge-card.js',
     format: 'es',
-    inlineDynamicImports: false,  // 关键：不要内联动态导入
+    inlineDynamicImports: true,
     sourcemap: false
   },
-  external: [
-    'https://unpkg.com/lit@2.8.0/index.js?module',
-    'https://unpkg.com/lit-html/directives/unsafe-html.js?module'
+  plugins: [
+    nodeResolve({ preferBuiltins: false })
   ]
 };
