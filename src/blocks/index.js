5aaca4e67b4e9ac9b39df43c44954d3e0c23ec38
蜘蛛饭 <39700758+938134@users.noreply.github.com>
2025-12-08 20:05:54 +0800

    Delete src/blocks/index.js

--- src/blocks/index.js
+++
@@ -1,5 +0,0 @@
-// blocks/index.js - 统一导出块相关功能
-export { BlockBase } from './block-base.js';
-export { BlockEditForm } from './block-edit-form.js';
-export { BlockManagement } from './block-management.js';
-export { AREAS, ENTITY_ICONS } from './block-config.js';
